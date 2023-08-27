ab063f985c86ca00e19c6b36a51e574f6875a7f9
Zellentin, Joshua Leander <joshua.zellentin@dlr.de>
2023-08-27 16:26:39 +0200

    added some comments

--- shepard-rocrate.py
+++ shepard-rocrate.py
@@ -1,21 +1,14 @@
 from shepard_client.api_client import ApiClient
 from shepard_client.configuration import Configuration
 from shepard_client.api.collection_api import CollectionApi
-from shepard_client.models.collection import Collection
 from shepard_client.api.data_object_api import DataObjectApi
-from shepard_client.models.data_object import DataObject
 from shepard_client.api.timeseries_api import TimeseriesApi
-from shepard_client.models.timeseries_container import TimeseriesContainer
-from shepard_client.models.timeseries_payload import TimeseriesPayload
-from shepard_client.models.timeseries import Timeseries
 from shepard_client.api.timeseries_reference_api import TimeseriesReferenceApi
 
 from rocrate.rocrate import ROCrate
 from rocrate.model.person import Person
 
-
 import json
-import inspect
 import mykey
 import os
 import ro_crate_contributers
@@ -37,7 +30,8 @@ timeseries_reference_api = TimeseriesReferenceApi(client)
 coll_id = int(input("Enter a Collection ID: "))
 collection = collection_api.get_collection(coll_id)
 
-### Collect all dataobjects linked to the Collection
+### Collect information into lists
+## Get all dataobject IDs for later
 def get_all_DO_IDs(coll_id):
     list_of_dataobjects = dataobject_api.get_all_data_objects(coll_id)
     id_list=[]
@@ -45,6 +39,7 @@ def get_all_DO_IDs(coll_id):
         id_list.append(i.__getattribute__('id'))
     return id_list
 
+## Get all timeseries references
 def get_all_TSR_IDs(coll_id, do_id, id_list):
     tsr_list = timeseries_reference_api.get_all_timeseries_references(coll_id, do_id)
     if not tsr_list:
@@ -53,13 +48,14 @@ def get_all_TSR_IDs(coll_id, do_id, id_list):
         for i in tsr_list:
             id_list.append([do_id, i.__getattribute__('id')])
 
+### Instantiate a list of timeseries references with the corresponding dataobject ID
 do_list = dataobject_api.get_all_data_objects(coll_id)
 do_id_list = get_all_DO_IDs(coll_id)
 tsr_id_list = []
 for i in do_id_list:
     get_all_TSR_IDs(coll_id, i, tsr_id_list)
 
-### Create the relationship tree
+### Create the relationship tree, not currently used
 prev_objects = []
 def find_ancestor(id):
     dataobject = dataobject_api.get_data_object(coll_id,id)
@@ -76,9 +72,7 @@ for i in do_id_list:
     if ancestor not in unique_ancestors:
         unique_ancestors.append(ancestor)
 
-### Create RO-Crate
-crate = ROCrate()
-
+### Creating the temporary files
 os.mkdir("./collection")
 os.mkdir("./dataobject")
 os.mkdir("./timeseriesreference")
@@ -107,11 +101,10 @@ for i in tsr_id_list:
             json.dump(timeseries[j].to_dict(), f, ensure_ascii=False, indent=4, sort_keys=True, default=str)
         f.close()
 
-
-input("Waiting...")
+### Create RO-Crate
+crate = ROCrate()
 
 ### Add data to the Crate
-
 crate.add_file("./collection/Collection.json", "collection.json", properties={
     "name" : collection.__getattribute__('name'),
     "author" : collection.__getattribute__('created_by'),
@@ -121,6 +114,7 @@ crate.add_dataset("./dataobject", "dataobject")
 crate.add_dataset("./timeseriesreference", "timeseriesreference")
 crate.add_dataset("./data", "data")
 
+### Add people to the crate
 for i in ro_crate_contributers.people:
     crate.add(Person(crate, i[2], properties={
         "name": i[0],
@@ -130,7 +124,6 @@ for i in ro_crate_contributers.people:
 crate.write("shepard_crate")
 
 ### Remove the temporary dictionaries
-
 for i in os.listdir("./dataobject"):
     os.remove("./dataobject/"+i)
 for i in os.listdir("./collection"):
